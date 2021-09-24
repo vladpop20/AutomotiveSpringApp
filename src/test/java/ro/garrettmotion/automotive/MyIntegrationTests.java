023dc4bc53cdac6cb59c56bef53cd37412454825
136830 <vlad.popescu@garrettmotion.com>
2021-09-24 18:24:14 +0300

    Made some adjustments to Integration tests.

--- src/test/java/ro/garrettmotion/automotive/MyIntegrationTests.java
+++ src/test/java/ro/garrettmotion/automotive/MyIntegrationTests.java
@@ -1,9 +1,13 @@
 package ro.garrettmotion.automotive;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,4 +20,13 @@ public class MyIntegrationTests {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetAllVehicles() {
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    }
 }
