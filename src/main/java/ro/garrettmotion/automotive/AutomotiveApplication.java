f1e68ba9e23cb6c0e2d30887bc224a652abf657f
vladpop20 <I@mAnG00dEngineer>
2021-10-16 12:39:28 +0300

    MAde something

--- src/main/java/ro/garrettmotion/automotive/AutomotiveApplication.java
+++ src/main/java/ro/garrettmotion/automotive/AutomotiveApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ro.garrettmotion.automotive"})
+//@ComponentScan(basePackages = {"ro.garrettmotion.automotive"})
 public class AutomotiveApplication {
 
 	public static void main(String[] args) {
